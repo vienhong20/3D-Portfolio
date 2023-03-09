855f694f7d099ab225b74b6243f8281645f40752
VIEN HONG <vienhong88@gmail.com>
2023-03-09 17:29:27 -0500

    Update Hero

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,21 @@
-import { Html, useProgress }  from '@react-three/drei';
-
+import { Html, useProgress } from '@react-three/drei';
 
 const Loader = () => {
   const { progress } = useProgress();
 
   return (
-   <Html>
-    <span className="canvas-load"></span>
-    <p style={{ fontSize: 14, color:'#f1f1f1', fontWeight: 800, marginTop: 40 }}
-    >{progress.toFixed(2)}%</p>
-   </Html>
+    <Html>
+      <span className="canvas-load"></span>
+      <p 
+      style={{
+        fontSize: 14,
+        color: '#f1f1f1',
+        fontWeight: 800,
+        marginTop: 40
+      }}
+      >{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
-export default Loader;
+export default Loader
