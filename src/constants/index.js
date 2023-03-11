3d0676cf43e2791685d681316b07b6866413177f
VIEN HONG <vienhong88@gmail.com>
2023-03-11 12:08:57 -0500

    Update Feedbacks.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -172,7 +172,7 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "I thought it was impossible to make a website as beautiful as our product, but Vince proved me wrong.",
       name: "Sara Lee",
       designation: "CFO",
       company: "Acme Co",
@@ -180,7 +180,7 @@ import {
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        "I've never met a web developer who truly cares about their clients' success like Vince does.",
       name: "Chris Brown",
       designation: "COO",
       company: "DEF Corp",
@@ -188,8 +188,8 @@ import {
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
+        "After Vince optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Jackie Nguyen",
       designation: "CTO",
       company: "456 Enterprises",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
